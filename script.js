610e7bb675b53ec899b6750b026cee868e54805e
NutanBhoyar <144129072+NutanBhoyar@users.noreply.github.com>
2024-01-06 17:49:22 +0530

    5 challenge solve

---
+++ script.js
@@ -0,0 +1,34 @@
+// Coding challenge #1: Print numbers from 1 to 10
+
+for(let i=1;i<11;i++)
+{
+    // console.log(i);
+}
+
+// Coding challenge #2: Print the odd numbers less than 100
+
+for(let i=1;i<100;i++)
+{
+     if(i%2!=0){
+        // console.log(i);
+     }   
+
+}
+
+// Coding challenge #3: Print the multiplication table with 7
+for(let i=1;i<11;i++){
+    // console.log(i*7);
+}
+
+
+// Coding challenge #4: Print all the multiplication tables with numbers from 1 to 10
+
+for(let i=1;i<11;i++){
+   for(let j=1;j<11;j++){
+    console.log(i*j);
+   }
+   console.log(" ")
+}
+
+
+// Coding challenge #5: Calculate the sum of numbers from 1 to 10
